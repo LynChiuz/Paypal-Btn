50fb770fb23b6bbe9041e9bb86b15f83139a191e
Hunghv <haviethung0801@gmail.com>
2024-09-21 13:41:32 +0700

    update

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 // Khởi chạy server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
